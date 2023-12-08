796606e6e87412b1e0120b56e005d58ddc9fb723
kai-lee1 <66237421+kai-lee1@users.noreply.github.com>
2023-12-08 14:38:03 -0800

    added placeTile in Player

--- Carcassonne/src/carcassonne/CarcassonneMain.java
+++ Carcassonne/src/carcassonne/CarcassonneMain.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 public class CarcassonneMain {
 	public static void main (String[] args) {
 		Board board = new Board(2);
-		for (int i = 0; i < 133; i++) {
+		for (int i = 0; i < 133; i++) {  //create tiles
 			for (int j = 0; j < 133; j++) {
 				for (int x : board.board[i][j].types)
 				System.out.print(x);
--- Carcassonne/src/carcassonne/Player.java
+++ Carcassonne/src/carcassonne/Player.java
@@ -1,5 +1,25 @@
 package carcassonne;
 
 public class Player {
+    public Player() {
+		
+    }
 
+    public boolean placeTile(int x, int y, Board board) {
+        if ((board.validSide(board.tiles.get(board.turnCount), board.board[x][y-1], 0)) && 
+        (board.validSide(board.tiles.get(board.turnCount), board.board[x+1][y], 1)) &&
+        (board.validSide(board.tiles.get(board.turnCount), board.board[x][y+1], 2)) &&
+        (board.validSide(board.tiles.get(board.turnCount), board.board[x-1][y], 3)) &&
+        (board.board[x][y].types[0] == -1) &&
+        ((board.board[x+1][y].types[0] != -1) || (board.board[x][y+1].types[0] != -1) || (board.board[x-1][y].types[0] != -1) || (board.board[x][y-1].types[0] != -1)) ) {
+            
+            board.board[x][y] = board.tiles.get(board.turnCount);
+            return true;
+        }
+        
+        return false;
+
+    }
+
+    
 }
